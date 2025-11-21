ed7c6979e8998a482a0133762d3282951b0ada2c
CoozillaX <79683806+CoozillaX@users.noreply.github.com>
2025-11-22 00:01:09 +1100

    add project information

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -2,15 +2,47 @@
   <div>
     <el-card shadow="hover">
       <template #header>
+        <div class="card-header">FrontEnd</div>
+      </template>
+      <div>
+        <span>
+          Powered by Vue3 + Vite + Element Plus, automatically deployed through
+          Cloudflare Pages.
+        </span>
+        <br /><br />
+        <span>
+          Projcet Repository:
+          <a href="https://github.com/Cynic158/Bunker-Web" target="_blank">
+            Click Me
+          </a>
+        </span>
+      </div>
+    </el-card>
+    <el-card shadow="hover" style="margin-top: 12px">
+      <template #header>
+        <div class="card-header">BackEnd</div>
+      </template>
+      <div>
+        <span>
+          Powered by Gin + Gorm, secured behind Cloudflare Reverse Proxy.
+        </span>
+        <br /><br />
+        <span>
+          Projcet Repository:
+          <a href="https://github.com/CoozillaX/bunker-web" target="_blank">
+            Click Me
+          </a>
+        </span>
+      </div>
+    </el-card>
+    <el-card shadow="hover" style="margin-top: 12px">
+      <template #header>
         <div class="card-header">Community</div>
       </template>
       <div>
         <span>您可以通过以下入口加入我们的社区：</span><br /><br />
         <span>
-          <a 
-            href="https://t.me/+cBXScYSrN9YxMzFl"
-            target="_blank"
-            >
+          <a href="https://t.me/+cBXScYSrN9YxMzFl" target="_blank">
             Telegram 群组
           </a>
         </span>
